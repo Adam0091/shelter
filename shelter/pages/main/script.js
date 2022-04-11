2c6b3d9e8fa8047abd7de5a037a705fe63315de1
Adam <adam.holo@mail.ru>
2022-04-11 11:55:26 +0400

    feat: add task shelter

---
+++ shelter/pages/main/script.js
@@ -0,0 +1,34 @@
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js';
+const result = await fetch('./pets.json');
+const PETS = await result.json();
+
+let domPetsHTML = document.querySelector(".swiper-wrapper");
+PETS.forEach(element => {
+    domPetsHTML.innerHTML += `
+    <div class="swiper-slide">
+    <div class="slide__content">
+        <div class="slide__wrapper-img">
+            <img src="${element.img}" alt="${element.name}" class="slide__img">
+        </div>
+        <div class="slide__pet-name">${element.name}</div>
+        <div class="slide__wrapper-btn">
+            <div class="slide__btn">Learn more</div>
+        </div>
+    </div>
+    </div>
+    `;
+});
+
+
+
+const swiper = new Swiper('.our-friends__slider', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 90,
+    simulateTouch: false,
+    navigation: {
+        nextEl: '.our-friend__button-next',
+        prevEl: '.our-friend__button-prev',
+        disabledClass: 'our-friend__button--disabled'
+    },
+});
